14eb54f54f6d19ab83eb1211fa3e72513bd0204e
tariqahmad0046-cpu <tariqahmad0046@gmail.com>
2026-04-20 00:00:04 +0500

    Update consumer.py

--- kafka-realtime-data-streaming/consumer.py
+++ kafka-realtime-data-streaming/consumer.py
@@ -1,1 +1,17 @@
+from kafka import KafkaConsumer
+import json
 
+consumer = KafkaConsumer(
+    'my-topic',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+print("Listening...\n")
+
+for message in consumer:
+    data = message.value
+
+    # just show summary instead of full image
+    print("Received row with", len(data), "pixels")
