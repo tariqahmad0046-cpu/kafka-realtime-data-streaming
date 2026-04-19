2b5fe2040add7d5df2d257c9f554cec40af18198
tariqahmad0046-cpu <tariqahmad0046@gmail.com>
2026-04-20 00:00:54 +0500

    Implement Kafka producer for real-time data streaming
    Added Kafka producer to send data from CSV to Kafka topic.

--- kafka-realtime-data-streaming/producer.py
+++ kafka-realtime-data-streaming/producer.py
@@ -1,1 +1,21 @@
+from kafka import KafkaProducer
+import pandas as pd
+import json
+import time
 
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# load your dataset
+df = pd.read_csv("dataset.csv")
+
+# send row by row
+for index, row in df.iterrows():
+    data = row.to_dict()
+
+    producer.send("my-topic", value=data)
+    print("Sent:", data)
+
+    time.sleep(1)  # simulate real-time
